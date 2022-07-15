509147e0b79e61cbcfabc0660ae22b166758bd72
RobNL <robbert.hessing@outlook.com>
2022-07-15 23:29:09 +0200

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+if [ ! -f "/etc/ssh/ssh_host_rsa_key" ]; then
+	# generate fresh rsa key
+	ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+fi
+if [ ! -f "/etc/ssh/ssh_host_dsa_key" ]; then
+	# generate fresh dsa key
+	ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
+fi
+
+#prepare run dir
+if [ ! -d "/var/run/sshd" ]; then
+  mkdir -p /var/run/sshd
+fi
+
+# Because kubernetes config maps cannot have a defined owner
+# just move authorized_keys.cmap to the correct location and set the correct user
+if [ -f "/home/python/.ssh/authorized_keys.cmap" ]; then
+  cp /home/python/.ssh/authorized_keys.cmap /home/python/.ssh/authorized_keys \
+  && chmod 600 /home/python/.ssh/authorized_keys \
+  && chown python:python /home/python/.ssh/authorized_keys
+fi
+
+/usr/sbin/sshd -D -e
